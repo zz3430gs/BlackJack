7744dca0d02ddb3914c015d6a1e3b675b708d021
zz3430gs <keigolis95@gmail.com>
2016-11-15 14:49:50 -0600

    added Rules class

---
+++ app/src/main/java/com/example/zz3430gs/blackjack/Rules.java
@@ -0,0 +1,20 @@
+package com.example.zz3430gs.blackjack;
+
+public class Rules{
+
+
+
+    public void overTwentyOne(card cardA, card cardB){
+
+        if (cardA + cardB > 21){
+
+        }
+    }
+    public void equalsTwentyOne(card cardA, card cardB){
+        if (cardA + cardB == 21){
+
+        }
+    }
+
+
+}
